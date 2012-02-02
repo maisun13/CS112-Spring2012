3ca56eaffa89194cb25495528d2a6c6412c8e27a
Mai Templeton <maisunniva@gmail.com>
2012-02-01 20:31:21 -0500

    hw02 done

---
+++ day3-4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+n=raw_input("Enter a number: ")
+m=raw_input("Enter a number: ")
+n=int(n)
+m=float(m)
+print n,"+",m,"=",n+m
+print n,"-",m,"=",n-m
+print n,"*",m,"=",n*m
+print n,"/",m,"=",n/m
---
+++ day3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+n=17
+pi=3.14159
+message="what ever!"
+message0="Valley girl says: "
+total=message0+message
+sixteen=n
+score=0
+score=score+5
+score+=5
+print total
+print message*50
+print sixteen
+print type(sixteen)
