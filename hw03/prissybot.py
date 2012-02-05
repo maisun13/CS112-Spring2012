740ee27b7319e2c4095300df65992fccc0b148c3
Mai Templeton <maisunniva@gmail.com>
2012-02-05 17:04:49 -0500

    hw03 done

--- hw03/prissybot.py
+++ hw03/prissybot.py
@@ -1,48 +1,64 @@
 #!/usr/bin/env python
-"""
-prissybot.py
 
-CS112 Homework 3:   PrissyBot
+print "Hey you! What's your name? "
+name=raw_input ()
 
-Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious questions, and says anything it likes.
-"""
+print "Ouch, thats an unfortunate name."
+print "hmmmmmmmmmm.... ",name
+print "How are you feeling then? "
+mood=raw_input ()
+print "Right....Well it's not as if i really care whether you're",mood
+print "or not. " 
 
-# Step 1:
-# -----------------------
-# Program the following.
-# 
-#    $ python prissybot.py
-#    Enter your name:  Paul
-#   
-#    PrissyBot: Hello there, Paul
-#    Paul: hi bot
-#    PrissyBot: You mean, "hi bot, sir!"
-# 
-# Make sure the user inputs their own name and responses.
+print "We're going to go along and solve some math equations. You got a problem with that?"
+problem=raw_input ()
+print "Whatever... like i care. "
 
+print "Try not to take TOO long. "
+n=raw_input ("enter a number: ")
+m=raw_input ("enter a number: ")
+n=int (n)
+m=float (m)
 
+print n,"+",m,"=",n+m
 
-# Step 2:
-# -----------------------
-# Keep adding to the conversation. Make sure that your program 
-# includes the following:
-# 
-#  * get and use input from the user
-#  * 3 math problems
-#     * at least one should get numbers from the user
-#  * at least 3 insults
+print "Well that was simple. How about this one? "
+n=raw_input ("enter a number: ")
+m=raw_input ("enter a number: ")
+n=int (n)
+m=float (m)
 
+print n,"-",m,"=",n-m
 
-# Advanced
-# -------------------------
-# Make sure your prissy bot uses string formatting throughout.  
-# Also, create new programs for the following:
-#  
-#  1. draw some kind of ascii art based on user input
-#  2. print a decimal/binary/hexidecimal conversion table 
-#     * well formated and labeled
-#     * reads 5 numbers from the input (all less than 256)
-#  3. reduce a fraction
-#     * read a numerator and denominator from the user
-#     * ex.  6/4 = 1 2/4
+print "Yeah Yeah Yeah. Try this. "
+n=raw_input ("enter a number: ")
+m=raw_input ("enter a number: ")
+n=int (n)
+m=float (m)
+
+print n,"*",m,"=",n*m
+
+print "Ugh... that took you long enough."
+
+print "Well despite the fact that i did most of the work, here's a little something for your so called.... efforts. "
+
+print "So whaddya want it to be made out of? Keep it simple and give me a symbol. "
+s=" "
+c=raw_input ()
+
+print 4*s,1*c
+print 3*s,3*c
+print 2*s,5*c
+print 1*s,7*c
+print 0*s,9*c
+print 1*s,7*c
+print 2*s,5*c
+print 3*s,3*c
+print 4*s,1*c
+
+print "Thats probably the most bling you'll ever see." 
+
+ 
+
+  
 
