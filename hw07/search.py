76dd010bbe7f93ac801bac4ab1d7161d2b187330
Mai Templeton <maisunniva@gmail.com>
2012-02-19 20:29:29 -0500

    hw07 done

--- hw07/search.py
+++ hw07/search.py
@@ -8,21 +8,68 @@ I used the Iterative implementation from here:
 """
 from hwtools import input_nums
 
-nums=inputNums()
-sorted(nums)
-print "I have sorted your numbers"
-x=raw_input("Which number should I find: ")
-m=1
-M=len(nums)-1
-while M>=m:
-    md=M+m/2
-    if nums[md]==x:
+while True:
+    try:
+        nums = input_nums() # User inputs a list of numbers. 
+
+        # To find any duplicate numbers.
+        for num in nums:
+            if nums.count(num) > 1:
+                raise NameError ('duplicate')
         break
-    elif x>nums[md]:
-       m=md+1
-     else:
-        M-=1
-if nums[md]=x:
-    print "Found", x "at", md
-else
-    print "Could not find", x
+
+    # Print an error statement if the user inputs something other than a number.
+    except ValueError:
+        print "Please enter numers."
+
+    # Print an error statement if the user inputs any duplicate numbers.     
+    except NameError:
+        print "No repeating numbers please!"
+
+
+nums_sort = sorted(nums) # User's sumbers are sorted into a list.
+print "I have sorted your numbers"
+print nums_sort
+
+while True:
+    try:
+        # User inputs a number to find in the list.
+        num_to_find = int(raw_input("Which number should I find: "))
+        break
+    # Print an error statement if user enters anything other than a number.
+    except ValueError:
+        print "Enter a number please."
+
+min_position = 0 # The minimum position in the list being checked. 
+
+max_position = len(nums_sort) - 1 # The maximum position in the list being checked.
+
+mid_position = (min_position + max_position) / 2 # The middle position in the list being checked. This is found by getting the average of the minimum and maximum position).
+
+# While the number to find is not equal to the middle position or if te minimum position is less than the maximum position:
+while (nums_sort[mid_position] != num_to_find) or (min_position < max_position):
+
+    # Check the posiiton to the right if the number to find is greater than the middle position (add one to the middle position).
+    if num_to_find > nums_sort[mid_position]:
+        min_position = mid_position + 1 
+
+        # Check the middle position again. 
+        mid_position = (min_position + max_position) / 2
+    # Check the position to the left if the number to find is less than the middle position (subtract one from the middle position).
+    elif num_to_find < nums_sort[mid_position]:
+        max_position = mid_position - 1
+
+        # Check the middle position again. 
+        mid_position = (min_position + max_position) / 2
+
+    # Break the while loop if the middle position matches the number to find or if the minimum position becomes greater than the maximum position. 
+    if (nums_sort[mid_position] == num_to_find) or (min_position > max_position):
+        break
+
+# If the middle position is equal to the number to find then print the the number to find has been found at it's position in the list. 
+if (nums_sort[mid_position] == num_to_find):
+    print "Found", num_to_find, "at position", mid_position + 1 , "in sorted list"
+
+# Print that the number couldn't be foun if the number could not be found at all. 
+else:
+    print "Could not find", num_to_find
--- hw07/sort.py
+++ hw07/sort.py
@@ -8,18 +8,35 @@ I used this:
 """
 from hwtools import input_nums
 
-nums = inputnums()
+while True:
+    try:
+        nums = input_nums() # User enters a list of numbers.
+        break
 
+    #Print an error message if the user enters anything other than a number.
+    except ValueError:
+        print "Enter numbers please."
+
+# The number list before it is sorted.
 print "Before sort:"
 print nums
 
-N=len(nums)-1
-for x in range(N)
-    p=x
-    for i in range(x+1 N):
-        if nums[i]<nums[p]:
-            pos=i
-   nums[x],nums[p]=nums[p],nums[x]
+max_position = len(nums) # The max position is the number of elements in the list. 
 
+for current_position in range(max_position):
+
+    current_min = current_position # The current minimum begins as the current position.
+
+    # This tests the position to the right of the last current position within the range of the max position.  
+    for test_position in range(current_position + 1 , max_position):
+
+        # If the number in the test position is less than that of the current minimum then the current minimum becomes the testing position. 
+        if nums[test_position] < nums[current_min]:
+            current_min = test_position
+
+        # This swaps the current minimum for the current position after each test of each position in the list. 
+        nums[current_position] , nums[current_min] = nums[current_min] , nums[current_position]
+
+# Prints the number list after it is sorted.
 print "After sort:"
 print nums
